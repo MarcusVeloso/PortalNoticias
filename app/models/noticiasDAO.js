f5db6e3675da654778ba312fcc2538e03b4befec
Marcus Veloso <desenvolvedoroficial@gmail.com>
2018-07-07 22:02:21 -0300

    Exibir as notícias em ordem desc

--- app/models/noticiasDAO.js
+++ app/models/noticiasDAO.js
@@ -11,7 +11,7 @@ NoticiasDAO.prototype.get5UltimasNoticias = function (callback) {
 }
 
 NoticiasDAO.prototype.getNoticias = function (callback) {
-    this._connection.query('SELECT * FROM noticias', callback);
+    this._connection.query('SELECT * FROM noticias ORDER BY data_criacao desc', callback);
 }   
 
 NoticiasDAO.prototype.salvarNoticia = function (noticia, callback) {    
